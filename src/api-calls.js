37b8259a809b2bc45df4762cfd6b85201875ed46
santiagodev <sanxp18@gmail.com>
2025-03-31 10:35:41 -0400

    refactor of generateMap function and set up messages when an error is being caught

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,5 +1,7 @@
 const API_KEY = "at_9mvHyuBDsjGFGRqUbQEUcvXsrVc8L";
 const API = "https://geo.ipify.org/api/v2/country,city?apiKey=";
+const mapContainer = document.getElementById("map");
+let map = null;
 
 async function getIPDetails(ip) {
     const userIpAddress = document.getElementById("ip-address-value");
@@ -14,7 +16,7 @@ async function getIPDetails(ip) {
             userTimezone.textContent = data.location.timezone;
             userIsp.textContent = data.isp === "" ? "No content available" : data.isp;
 
-            generateMap(data.location.lat, data.location.lng)
+            generateMap(data.location.lat, data.location.lng);
         };
 
         const url = ip ? `${API}${API_KEY}&ipAddress=${ip}` : `${API}${API_KEY}`;
@@ -24,27 +26,47 @@ async function getIPDetails(ip) {
         
     } catch (error) {
         console.error(error);
-        userIpAddress.textContent = "Enter a valid address ❌";
-        userLocation.textContent = "Enter a valid address ❌";
-        userTimezone.textContent = "Enter a valid address ❌";
-        userIsp.textContent = "Enter a valid address ❌";
-        const mapContainer = document.getElementById("map");
-        mapContainer.innerText = "Enter a valid address ❌";
-        mapContainer.classList.add("error-map-container");
+        userIpAddress.textContent = "No content available ❌";
+        userLocation.textContent = "No content available ❌";
+        userTimezone.textContent = "No content available ❌";
+        userIsp.textContent = "No content available ❌";
+        const main = document.querySelector("main");
+        const errorMessage = document.createElement("p");
+        mapContainer.classList.add("hide");
+        errorMessage.textContent = "Enter a valid address ❌";
+        errorMessage.classList.add("error-map");
+        main.appendChild(errorMessage);
     }
 }
 
 async function generateMap(lat, lgt) {
-    // const mapContainer = document.getElementById("map");
-    // mapContainer.innerHTML = "";
-    const map = L.map('map').setView([lat, lgt], 13);
+    mapContainer.classList.remove("hide");
+    const errorMessage = document.querySelector(".error-map");
+        if(errorMessage) {
+            errorMessage.remove();
+        }
+
+    if (!map) {
+        // Inicializar el mapa si no existe
+        map = L.map('map').setView([lat, lgt], 13);
+    } else {
+        // Actualizar la vista del mapa existente
+        map.setView([lat, lgt], 13);
+
+        // Eliminar todas las capas existentes del mapa
+        map.eachLayer((layer) => {
+            map.removeLayer(layer);
+        });
+    }
+
+    // Agregar una nueva capa de tileLayer
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    const marker = L.marker([lat, lgt]).addTo(map);
-    
+    // Agregar un nuevo marcador
+    L.marker([lat, lgt]).addTo(map);
 }
 
 export { getIPDetails };
